6e2961db331abd2a431a9b59ea4aff9f657cd7d9
Guido Rodriguez <guido292001@gmail.com>
2020-06-18 21:04:23 -0300

    Se solucionó error de sintaxis en la dirección del archivo .

--- GUI/0.0.1/src/mapa/mapaScript.js
+++ GUI/0.0.1/src/mapa/mapaScript.js
@@ -18,7 +18,7 @@ const globoMarcador = L.icon({
 
 //Se crea el objeto baseMarcador cuya funcion es la de marcador en el mapa para la base terrena
 const baseMarcador = L.icon({
-    iconUrl: "../../../resources/icons/BaseMarcador1.pnvg",
+    iconUrl: "../../../resources/icons/BaseMarcador1.svg",
     iconSize: [50, 50],
 });
 
