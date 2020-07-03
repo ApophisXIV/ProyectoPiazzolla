43e201251db73186583f69b9e757323ee54a8cde
Guido Rodriguez <guido292001@gmail.com>
2020-07-03 08:55:30 -0300

    Create index_1_renderer.js

---
+++ GUI/0.0.1/src/init_Screen/index_1_renderer.js
@@ -0,0 +1,49 @@
+
+//Evento al presionar la barra espaciadora (Evitar que el slider selector conmute sin generar ninguna acción)
+window.onkeydown = function(teclaPresionada){
+
+    //Si la tecla que se presiono es la barra espaciadora
+    if(teclaPresionada.keyCode == 32) {
+
+        //El evento se cancela y se evita que la barra accione el slider
+        teclaPresionada.preventDefault();
+    }
+};
+
+
+function Modo_Dark_Light(){
+
+
+    var colorActual=document.body.style.backgroundColor;
+
+    //Cambio de blanco a violeta
+    if(colorActual!="rgb(51, 0, 102)"){
+
+        document.body.style.backgroundColor="#330066";
+        document.body.style.transition="0.4s";                
+        document.getElementById("tituloPiazzollaCARD").style.color="#FFFFFF";
+        document.getElementById("cuerpoPiazzollaCARD").style.color="#FFFFFF";
+        document.getElementById("cardPiazzolla").style.backgroundColor="#5400AA";
+        document.getElementById("cardPiazzolla").style.transition="0.4s";                
+        document.getElementById("botonStart").style.backgroundColor="#7F00FF";
+        document.getElementById("botonStart").style.borderColor="#7F00FF";
+        document.getElementById("botonStart").style.transition="0.4s";                
+        document.getElementById("LogoPIAZZOLLA").src="../../../resources/images/PiazzollaLogoDarkR.png";
+    }
+
+    //Cambio de violeta a blanco
+    if(colorActual=="rgb(51, 0, 102)"){
+
+        document.body.style.backgroundColor="#FFFFFF";
+        document.body.style.transition="0.4s";
+        document.getElementById("tituloPiazzollaCARD").style.color="#000000";
+        document.getElementById("cuerpoPiazzollaCARD").style.color="#000000";       
+        document.getElementById("cardPiazzolla").style.backgroundColor="#FFFFFF";         
+        document.getElementById("cardPiazzolla").style.transition="0.4s";                
+        document.getElementById("botonStart").style.backgroundColor="#007bff";                
+        document.getElementById("botonStart").style.borderColor="#007bff";
+        document.getElementById("botonStart").style.transition="0.4s";                
+        document.getElementById("LogoPIAZZOLLA").src="../../../resources/images/PiazzollaLogoLightR.png";
+    }
+
+}
