33169a8f90300ff4e0ba04ecb2c8c0de6200bbc7
Guido Rodriguez <guido292001@gmail.com>
2020-07-03 09:06:39 -0300

    Update. Se habilita el canal de recepción desde el proceso renderer mediante IPC. Se recepciona solicitud para cambiar el archivo index_1.html por gridScreen.html al presionar el boton Start

--- GUI/0.0.1/app/js/main.js
+++ GUI/0.0.1/app/js/main.js
@@ -2,24 +2,32 @@
 const electron = require('electron');
 const path = require('path');
 const url = require('url');
-const { request } = require('http');
 
 // SET ENV
 process.env.NODE_ENV = 'development';
 process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = '1';
 
-const {app, BrowserWindow, Menu} = electron;
+const {app, BrowserWindow, Menu, ipcMain} = electron;
 
 let homeWindow;
 
 function VENTANA_HOME() {
 
   //Ventana principal
-  homeWindow = new BrowserWindow({webPreferences:{nodeIntegration: true}, width: 640, height: 480, transparent: false, frame: true});
+  homeWindow = new BrowserWindow({
   
-  //Debug, el path orignal era incorrecto.
-  //console.log(path.join(__dirname, `../public/html/index.html`));
+    webPreferences:{nodeIntegration: true}, 
+    
+    width: 640, 
+    
+    height: 480, 
+    
+    icon: path.join(__dirname, '../../resources/icons/boya.png')
+    
+  });
 
+  
+  //Se carga el archivo html de la ventana
   homeWindow.loadURL(url.format({
 
     pathname: path.join(__dirname, `../public/html/index_1.html`),
@@ -28,12 +36,35 @@ function VENTANA_HOME() {
     
   }));
 
+  //Se maximiza la ventana
   homeWindow.maximize();
+
+  //Se recibe un mensaje asincrono desde el proceso renderer
+  ipcMain.on("asynchronous-message",(eventoRENDER,mensajeCLOSE)=>{
+
+    //Si el mensaje se corresponde con el mensaje esperado entonces se cambia el archivo html de la ventana 
+    if(mensajeCLOSE=="CAMBIAR_VENTANA_HOME"){
+
+      //DEBUG
+      //console.log(mensajeCLOSE);
+
+      //Se carga el archivo correspondiente a la nueva ventana luego de pulsar el boton START
+      homeWindow.loadURL(url.format({
+
+        pathname: path.join(__dirname, `../public/html/gridScreen.html`),
+        protocol: 'file',
+        slashes: true
+        
+      }));
+
+    }
+
+  })
+
 }
 
 //Una vez este listo para mostrarse el contenido se llama a la funcion VENTANA_HOME para generarlas
-app.on('ready', VENTANA_HOME);
-
+app.whenReady().then(VENTANA_HOME);
 
 //Codigo para cerrar el proceso correctamente en MAC
 app.on('window-all-closed', () => {
