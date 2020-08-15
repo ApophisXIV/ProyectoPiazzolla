db36111e43e6e67d4d00c1e6a5d1de541120ca8e
Guido Rodriguez <guido292001@gmail.com>
2020-08-15 15:18:25 -0300

    Se actualiza el renderer a los nuevos cambios realizados en index.html. Cambios en el esquema de colores.

--- GUI/0.0.1/src/init_Screen/index_renderer.js
+++ GUI/0.0.1/src/init_Screen/index_renderer.js
@@ -31,35 +31,45 @@ function Modo_Dark_Light(){
     var colorActual=document.body.style.backgroundColor;
 
     //Cambio de blanco a violeta
-    if(colorActual!="rgb(51, 0, 102)"){
+    if(colorActual!="rgb(20, 15, 42)"){
 
-        document.body.style.backgroundColor="#330066";
+        document.body.style.backgroundColor="#140F2A";
         document.body.style.transition="0.4s";                
         document.getElementById("tituloPiazzollaCARD").style.color="#FFFFFF";
-        document.getElementById("cuerpoPiazzollaCARD").style.color="#FFFFFF";
-        document.getElementById("cardPiazzolla").style.backgroundColor="#5400AA";
+        document.getElementById("subtituloPiazzollaCARD").style.color="#FFFFFF";
+        document.getElementById("descripcionPiazzollaCARD").style.color="#FFFFFF";
+        document.getElementById("divisionPiazzollaCARD").style.borderTopColor="#FFFFFF"; 
+        document.getElementById("containerCARD").style.borderColor="#353251"; 
+        document.getElementById("containerCARD").style.boxShadow="rgba(65, 56, 100, 0.5) 0px 0px 20px 1rem"; 
+
+        document.getElementById("cardPiazzolla").style.backgroundColor="#212432";
         document.getElementById("cardPiazzolla").style.transition="0.4s";                
-        document.getElementById("botonStart").style.backgroundColor="#7F00FF";
-        document.getElementById("botonStart").style.borderColor="#7F00FF";
+        document.getElementById("botonStart").style.backgroundColor="#454A5A";
+        document.getElementById("botonStart").style.borderColor="#454A5A";
         document.getElementById("botonStart").style.transition="0.4s";    
         document.head.appendChild(botonStart_DARK_focus);       
-        document.getElementById("LogoPIAZZOLLA").src="../../../resources/images/PiazzollaLogoDarkR.png";
+        // document.getElementById("LogoPIAZZOLLA").src="../../../resources/images/PiazzollaLogoDarkR.png";
     }
 
     //Cambio de violeta a blanco
-    if(colorActual=="rgb(51, 0, 102)"){
+    if(colorActual=="rgb(20, 15, 42)"){
 
         document.body.style.backgroundColor="#FFFFFF";
         document.body.style.transition="0.4s";
-        document.getElementById("tituloPiazzollaCARD").style.color="#000000";
-        document.getElementById("cuerpoPiazzollaCARD").style.color="#000000";       
-        document.getElementById("cardPiazzolla").style.backgroundColor="#FFFFFF";         
+        document.getElementById("tituloPiazzollaCARD").style.color="#212529";
+        document.getElementById("subtituloPiazzollaCARD").style.color="#212529";
+        document.getElementById("descripcionPiazzollaCARD").style.color="#212529";   
+        document.getElementById("divisionPiazzollaCARD").style.borderTopColor="#D1D4D7"; 
+        document.getElementById("containerCARD").style.borderColor="#FFFFFF"; 
+        document.getElementById("containerCARD").style.boxShadow="rgba(29, 33, 36, 0.7) 0px 0px 20px 0rem"; 
+        
+        document.getElementById("cardPiazzolla").style.backgroundColor="#E9ECEF";         
         document.getElementById("cardPiazzolla").style.transition="0.4s";                
         document.getElementById("botonStart").style.backgroundColor="#007bff";                
         document.getElementById("botonStart").style.borderColor="#007bff";
         document.getElementById("botonStart").style.transition="0.4s"; 
         document.head.appendChild(botonStart_LIGHT_focus);
-        document.getElementById("LogoPIAZZOLLA").src="../../../resources/images/PiazzollaLogoLightR.png";
+        // document.getElementById("LogoPIAZZOLLA").src="../../../resources/images/PiazzollaLogoLightR.png";
     }
 
 
