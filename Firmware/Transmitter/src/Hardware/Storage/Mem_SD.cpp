13e9fab503a5fcdc5011010683916561e2291834
Guido Rodriguez <guido292001@gmail.com>
2020-09-15 07:23:34 -0300

    Update Mem_SD.cpp

--- Firmware/Transmitter/src/Hardware/Storage/Mem_SD.cpp
+++ Firmware/Transmitter/src/Hardware/Storage/Mem_SD.cpp
@@ -4,6 +4,20 @@
 
     #include <Hardware/Storage/Mem_SD.h>
 
+    void SetGPIO();
 
+    bool selfTest();
+
+    void formatoArchivo(bool formato_CSV_JSON);
+
+    void saveTemperatura(float temperatura);
+
+    void savePresion(float presion);
+
+    void saveHumedad(float humedad);
+
+    void saveVBateria(float tensionBateria);
+
+    void saveVLDO(float tensionLDO);
 
 #endif
