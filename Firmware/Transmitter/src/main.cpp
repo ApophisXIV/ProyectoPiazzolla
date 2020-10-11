d9bdfd1811423d585226a3b99bd200f6bb829725
Guido Rodriguez <guido292001@gmail.com>
2020-10-10 23:54:20 -0300

    Update main.cpp

--- Firmware/Transmitter/src/main.cpp
+++ Firmware/Transmitter/src/main.cpp
@@ -1,12 +1,34 @@
-#include <Arduino.h>
+#include <Hardware/Hardware.h>
 
-int main (void){
+int main (){
 
+    Hardware_Tests PCB_Transmisor;
+
+    PCB_Transmisor.prueba_1();
+
+    // Error_Monitor Monitor;
     
+    // USBSerial USB_TX;
 
-    while (1) {
+    // USB_TX.begin(115200);
+   
+    // pinMode(PC13,1);
 
+    // Monitor.setGPIO();
 
-    }
+    // while (!USB_TX){   
+    //     Monitor.BarridoUSB_Conectando();
+    // }
+
+    // if(USB_TX){
+    //     digitalWrite(PC13,1);
+    //     Monitor.BeepBeepAll(200);
+    //     USB_TX.println(F("USB Conectado OK"));
+    // }
+
+    // FlashMicro.write(0,3);
+
+    // USB_TX.println(F(FlashMicro.read(0)));
+
 
 }
