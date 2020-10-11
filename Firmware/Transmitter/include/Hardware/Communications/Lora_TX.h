dd94df12ed861609d2e44f8d00371d1b016b42a9
Guido Rodriguez <guido292001@gmail.com>
2020-10-11 00:57:46 -0300

    Clase LoRa_TX
    Comentario: En el proximo commit desarrollar herencia de metodos protected de Error_Monitor para la indicacion de errores y publics de LDO_Reg para la inicializacion del regulador LDO.

---
+++ Firmware/Transmitter/include/Hardware/Communications/Lora_TX.h
@@ -0,0 +1,73 @@
+ #ifndef __LoRa_TX_H__
+
+    #define __LoRa_TX_H__
+
+    #include <Hardware/Hardware_usage.h>
+
+    #include <Hardware/GPIO.h>
+
+    #include <Hardware/Utilities/delayNB.h>
+
+    #include <RadioLib.h>
+
+    class LoRa_TX{
+        
+        SX1276 sx1276 = new Module(CS_RF,DIO0,RESET_RF,DIO1);
+
+        USBSerial usbLora;
+
+        public:
+
+        void setGPIO();
+
+        uint8_t selfTest();
+
+        void sendTemperatura();
+
+        void sendPresion();
+            
+        void sendOrientacion();
+
+        void sendVLDO();
+
+        void sendVBatt();
+        
+        private:
+
+        struct echoTestVars{
+
+            uint8_t numeroAleatorioTX[1] = {0};
+
+            uint8_t numeroAleatorioRX[1] = {0};
+
+            bool TX_RX_estadoActual = 0; //TX->0 RX->1. Inicia en TX
+            
+            bool RX_SetOnce = 0; //Se configura en modo RX una sola vez
+                    
+
+        };
+
+        echoTestVars *tempVarEchoTest = new echoTestVars;
+
+        static volatile bool flagPaquete;   //Se define el flag de accion del paquete. 
+                                            //Tiene que inicializarse antes de usarse.
+
+        static volatile bool habilitacionISR;   //Se define la habilitacion de la ISR disparada por DIO0. 
+                                                //Tiene que inicializarse antes de usarse.
+
+        bool echoTest();
+
+        bool cambiarReceptor();
+
+        static void LoRa_ISR_DIO0();    //Tiene que ser static porque 
+                                        //no puede perderse la direccion 
+                                        //de memoria ni el contenido de 
+                                        //la funcion por mas que se cambie 
+                                        //el scope y tiene que ser accesible por 
+                                        //mas que un objeto de la clase 
+                                        //no se haya creado como ocurre con el
+                                        //uso iterno al setear la accion de DIO0
+
+    };
+
+#endif
