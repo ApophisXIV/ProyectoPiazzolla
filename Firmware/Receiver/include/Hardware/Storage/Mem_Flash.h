8a83a97835c7e09f0b1482114e978c8305a544ec
Guido Rodriguez <guido292001@gmail.com>
2020-10-10 23:03:50 -0300

    Create Mem_Flash.h
    Clase MEM_Flash. Metodo selfTest para auto-validacion

---
+++ Firmware/Receiver/include/Hardware/Storage/Mem_Flash.h
@@ -0,0 +1,17 @@
+#ifndef __Mem_Flash_H__
+
+    #define __Mem_Flash_H__
+
+    #include <Hardware/Hardware_usage.h>
+
+    #include <EEPROM.h>
+
+    class MEM_Flash{
+
+        public:
+        
+        bool selfTest();
+
+    };
+
+#endif
