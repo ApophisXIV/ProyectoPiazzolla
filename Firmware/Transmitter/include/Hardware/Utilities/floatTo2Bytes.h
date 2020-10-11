0bbe14314d3cf44976c018070e6da56e581b33f6
Guido Rodriguez <guido292001@gmail.com>
2020-10-11 00:07:28 -0300

    Funcion para el cast de float a uint16

---
+++ Firmware/Transmitter/include/Hardware/Utilities/floatTo2Bytes.h
@@ -0,0 +1,9 @@
+#ifndef __floatTo2Bytes_H__
+
+    #define __floatTo2Bytes_H__
+
+    #include <Hardware/Hardware_usage.h>
+
+    uint16_t conversionAndRedondeo(float_t &conversionFloatTo2Bytes);
+
+#endif
