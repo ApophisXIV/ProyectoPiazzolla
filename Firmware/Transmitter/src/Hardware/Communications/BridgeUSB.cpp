95c57d4493d80733a47d89607e75b1b52b953dd8
Guido Rodriguez <guido292001@gmail.com>
2020-10-22 22:00:08 -0300

    Create BridgeUSB.cpp

---
+++ Firmware/Transmitter/src/Hardware/Communications/BridgeUSB.cpp
@@ -0,0 +1,28 @@
+ #ifndef __BridgeUSB_CPP__
+
+    #define __BridgeUSB_CPP__
+
+    #include <Hardware/Communications/BridgeUSB.h>
+
+    USB_PORT_TX::USB_PORT_TX (){
+
+    }
+
+    void USB_PORT_TX::inicializar(){
+
+      /*  USB_TX.begin();
+
+        while (!USB_TX){   
+
+            USB_PORT_TX::Error_Monitor::barridoUSB_Conectando();
+        }
+
+        if(USB_TX){
+
+            USB_PORT_TX::Error_Monitor::beepBeepAll(50);
+            USB_TX.println("USB Conectado OK");
+        }*/
+    }
+
+
+#endif
