9b2d87124a25c3a59a15a7609ae0be89fdd2ef90
Guido Rodriguez <guido292001@gmail.com>
2020-10-22 22:00:12 -0300

    Update LDO.cpp

--- Firmware/Transmitter/src/Hardware/Analog/LDO.cpp
+++ Firmware/Transmitter/src/Hardware/Analog/LDO.cpp
@@ -4,19 +4,39 @@
 
     #include <Hardware/Analog/LDO.h>
 
+    LDO_Reg::LDO_Reg(){
+        
+        LDO_Reg::setGPIO();
+                
+        #ifdef DEBUG_LDO
+            LDO_Reg::UART3_DEBUG::printf(F("GPIO LDO Seteado\n"));
+            LDO_Reg::UART3_DEBUG::flush();
+        #endif
+
+    }
+
     void LDO_Reg::setGPIO(){
 
         pinMode(LDO_ENABLE,1);
         pinMode(LDO_ADC,4); // 4 = Entrada analogica -> Canal del ADC
+        analogReadResolution(12);
     }
 
     bool LDO_Reg::selfTest(){
 
         digitalWrite(LDO_ENABLE,1);
         
-        //HAL_GPIO_WritePin(GPIOB,5,GPIO_PIN_SET);
+        #ifdef DEBUG_LDO
+            LDO_Reg::UART3_DEBUG::printf(F("LDO ENABLE: 1. VLDO: %lu\n"),analogRead(LDO_ADC));
+            LDO_Reg::UART3_DEBUG::flush();
+        #endif
 
-        if(ADC_RES*analogRead(LDO_ADC)<=3.00){//~90% de la tensión nominal de trabajo del regulador 
+        if(analogRead(LDO_ADC)<=3723){//~90% de la tensión nominal de trabajo del regulador 
+
+            #ifdef DEBUG_LDO
+                LDO_Reg::UART3_DEBUG::printf(F("LDO Estado: Falla. Menor tension de la esperada\n"));
+                LDO_Reg::UART3_DEBUG::flush();
+            #endif
 
             digitalWrite(LDO_ENABLE,0);
 
@@ -27,8 +47,18 @@
         else{
 
             digitalWrite(LDO_ENABLE,0);
+            
+            #ifdef DEBUG_LDO
+                LDO_Reg::UART3_DEBUG::printf(F("LDO ENABLE: 0. VLDO: %lu\n"),analogRead(LDO_ADC));
+                LDO_Reg::UART3_DEBUG::flush();
+            #endif
 
-            if(ADC_RES*analogRead(LDO_ADC)>=0.33){//~10% de la tensión nominal de trabajo del regulador 
+            if(analogRead(LDO_ADC)>=409){//~10% de la tensión nominal de trabajo del regulador 
+
+                #ifdef DEBUG_LDO
+                    LDO_Reg::UART3_DEBUG::printf(F("LDO Estado: Falla. Mayor tension de la esperada\n"));
+                    LDO_Reg::UART3_DEBUG::flush();
+                #endif
 
                 digitalWrite(LDO_ENABLE,0); //Reintenta apagar el regulador
 
@@ -38,6 +68,13 @@
 
             else{
 
+                #ifdef DEBUG_LDO
+                    LDO_Reg::UART3_DEBUG::printf(F("LDO Estado: OK. Prueba exitosa!!\n"));
+                    LDO_Reg::UART3_DEBUG::flush();
+                #endif
+
+                digitalWrite(LDO_ENABLE,1);
+                
                 return (0);
             }
 
