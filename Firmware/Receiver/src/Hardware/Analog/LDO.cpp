7429b79116d6086eb755bf4d33bcd667951e176f
Guido Rodriguez <guido292001@gmail.com>
2020-10-22 22:01:56 -0300

    Update LDO.cpp

--- Firmware/Receiver/src/Hardware/Analog/LDO.cpp
+++ Firmware/Receiver/src/Hardware/Analog/LDO.cpp
@@ -5,7 +5,13 @@
     #include <Hardware/Analog/LDO.h>
 
     LDO_Reg::LDO_Reg(){
+
+        LDO_Reg::setGPIO();
         
+        #ifdef DEBUG_LDO
+            LDO_Reg::USBSerial::printf(F("GPIO LDO Seteado\n"));
+            LDO_Reg::USBSerial::flush();
+        #endif
     }
 
     void LDO_Reg::setGPIO(){
@@ -17,12 +23,19 @@
 
     bool LDO_Reg::selfTest(){
 
-        LDO_Reg::setGPIO();
-
         digitalWrite(LDO_ENABLE,1);
-            
+       
+        #ifdef DEBUG_LDO
+            LDO_Reg::UART3_DEBUG::printf(F("LDO ENABLE: 1. VLDO: %lu\n"),analogRead(LDO_ADC));
+            LDO_Reg::UART3_DEBUG::flush();
+        #endif
 
-        if((float_t)(ADC_RES*analogRead(LDO_ADC))<=3.00){//~90% de la tensión nominal de trabajo del regulador 
+        if(analogRead(LDO_ADC)<=3723){//~90% de la tensión nominal de trabajo del regulador 
+            
+            #ifdef DEBUG_LDO
+                LDO_Reg::UART3_DEBUG::printf(F("LDO Estado: Falla. Menor tension de la esperada\n"));
+                LDO_Reg::UART3_DEBUG::flush();
+            #endif
 
             digitalWrite(LDO_ENABLE,0);
 
@@ -34,7 +47,17 @@
 
             digitalWrite(LDO_ENABLE,0);
 
-            if((float_t)(ADC_RES*analogRead(LDO_ADC))>=0.33){//~10% de la tensión nominal de trabajo del regulador 
+            #ifdef DEBUG_LDO
+                LDO_Reg::UART3_DEBUG::printf(F("LDO ENABLE: 0. VLDO: %lu\n"),analogRead(LDO_ADC));
+                LDO_Reg::UART3_DEBUG::flush();
+            #endif
+
+            if(analogRead(LDO_ADC)>=409){//~10% de la tensión nominal de trabajo del regulador 
+
+                #ifdef DEBUG_LDO
+                    LDO_Reg::UART3_DEBUG::printf(F("LDO Estado: Falla. Mayor tension de la esperada\n"));
+                    LDO_Reg::UART3_DEBUG::flush();
+                #endif
 
                 digitalWrite(LDO_ENABLE,0); //Reintenta apagar el regulador
 
@@ -44,6 +67,13 @@
 
             else{
 
+                #ifdef DEBUG_LDO
+                    LDO_Reg::UART3_DEBUG::printf(F("LDO Estado: OK. Prueba exitosa!!\n"));
+                    LDO_Reg::UART3_DEBUG::flush();
+                #endif
+
+                digitalWrite(LDO_ENABLE,1);
+                
                 return (0);
             }
 
