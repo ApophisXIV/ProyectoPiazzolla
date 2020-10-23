d7e61119fe57cb855ce2dcc1f95c5ea662a45946
Guido Rodriguez <guido292001@gmail.com>
2020-10-22 22:00:39 -0300

    Update Hardware.h

--- Firmware/Transmitter/include/Hardware/Hardware.h
+++ Firmware/Transmitter/include/Hardware/Hardware.h
@@ -3,19 +3,23 @@
     #define __Hardware_H__
 
     #include <Hardware/Diagnostic/Monitor.h>
-    #include <Hardware/Storage/Mem_SD.h>
-    #include <Hardware/Storage/Mem_Flash.h>
+    //#include <Hardware/Communications/BridgeUSB.h>
+    //#include <Hardware/Storage/Mem_SD.h>
+    //#include <Hardware/Storage/Mem_Flash.h>
     #include <Hardware/Communications/LoRa_TX.h>
+    #include <Hardware/Communications/UART_debug.h>
 
-    class Hardware_Tests {
+    class Hardware_Tests{
         
         Error_Monitor MonitorLED;
         
-        MEM_Flash Flash;
+        //MEM_Flash Flash;
 
-        USBSerial USB_TX;
+        UART3_DEBUG Uart3Comm;
 
-        SD_CARD TarjetaSD;
+        //USB_PORT_TX USBComm;
+
+        //SD_CARD TarjetaSD;
 
         LoRa_TX LoRaComm;
 
